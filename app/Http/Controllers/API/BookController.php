0e6291b05508499d2d728f310a27c09456b0083b
tle22 <86483943+tle22@users.noreply.github.com>
2022-01-29 23:33:41 +0700

    first commit

---
+++ app/Http/Controllers/API/BookController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Book::get();
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $requestData = $request->all();
+        $book = Book::create($requestData);
+        return ["success" => true, "data" => $book];
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Book::findOrFail($id);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $requestData = $request->all();
+        $book = Book::findOrFail($id);
+        $success = $book->update($requestData);
+        return ["success" => $success];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $book = Book::findOrFail($id);
+        $success = $book->delete();
+        return ["success" => $success];
+    }
+}
